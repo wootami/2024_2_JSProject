b98113a64417f6e56110a420c450ff8f335cdff9
Red-Bod7120 <pjh93100@gmail.com>
2024-11-24 14:29:38 +0900

    auth

---
+++ scr/auth.js
@@ -0,0 +1,28 @@
+import { auth } from './firebase-config.js';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+
+// 회원가입 함수
+export const register = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // 회원가입 성공
+            console.log('회원가입 성공:', userCredential.user);
+        })
+        .catch((error) => {
+            // 오류 처리
+            console.error('회원가입 오류:', error.message);
+        });
+};
+
+// 로그인 함수
+export const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // 로그인 성공
+            console.log('로그인 성공:', userCredential.user);
+        })
+        .catch((error) => {
+            // 오류 처리
+            console.error('로그인 오류:', error.message);
+        });
+};
