e9367e05536b6fcb9a1e65210794ecb4643aa05c
Red-Bob7120 <pjh93100@gmail.com>
2024-11-26 14:01:29 +0900

    21554

---
+++ login.js
@@ -0,0 +1,11 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBfWIHpWVdl89Zho_L5BSBeE1wLsq0Pf6E",
+    authDomain: "test-433f5.firebaseapp.com",
+    projectId: "test-433f5",
+    storageBucket: "test-433f5.firebasestorage.app",
+    messagingSenderId: "899398454867",
+    appId: "1:899398454867:web:5c4b26ec46e238ec58c0a6"
+  };
+  const app = initializeApp(firebaseConfig);
