ec06f59d742d01ed9cfdbebc9b5f4df4c38686fd
Red-Bob7120 <pjh93100@gmail.com>
2024-12-03 15:55:12 +0900

    수정

--- FrontEnd/src/js/firebase-config.js
+++ FrontEnd/src/js/firebase-config.js
@@ -1,4 +1,3 @@
-// firebase-config.js (브라우저 환경용)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
@@ -7,7 +6,7 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-
 const firebaseConfig = {
   apiKey: "AIzaSyBdTMSJuAXluGdrQk6aAxPTszRme0YbKfA",
   authDomain: "jsproject-e6692.firebaseapp.com",
-  
+  //firebase 데이터 베이스
   projectId: "jsproject-e6692",
   storageBucket: "jsproject-e6692.appspot.com", 
   appId: "1:847960636309:web:06e0cdc6327acbcf66949c",
